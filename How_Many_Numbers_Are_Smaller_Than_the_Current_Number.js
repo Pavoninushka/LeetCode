d35758161a28bc6baf516e5879bc69e410b4a13c
Galina Karateeva <karateewa.galina@yandex.ru>
2021-04-10 16:12:34 +0200

    Changes by jetbrains

--- How_Many_Numbers_Are_Smaller_Than_the_Current_Number.js
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.js
@@ -2,16 +2,36 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// const smallerNumbersThanCurrent = function(nums) {
+//   let result = Array(nums.length).fill(0);
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] > nums[j]) {
+//         result[i]++;
+//       } else if (nums[i] < nums[j]) {
+//         result[j]++;
+//       }
+//     }
+//   }
+//   return result;
+// };
+//
+// console.log(smallerNumbersThanCurrent([8,1,2,2,3]));
+// [4,0,1,1,3]
+  // [1, 2, 2, 3, 8]
+
 const smallerNumbersThanCurrent = function(nums) {
-  let result = Array(nums.length).fill(0);
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] > nums[j]) {
-        result[i]++;
-      } else if (nums[i] < nums[j]) {
-        result[j]++;
-      }
+  let sortedArray = [...nums].sort((a, b) => a - b);
+  let result = [];
+  let indexes = new Map();
+
+  for (let i = 0; i < sortedArray.length; i ++) {
+    if (!indexes.has(sortedArray[i])) {
+      indexes.set(sortedArray[i], i);
     }
+  }
+  for (let element of nums) {
+    result.push(indexes.get(element));
   }
   return result;
 };
